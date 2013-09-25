c092ad59e6ec76edb1382efad32b26781149cbfc
stephencreighton <stephen.creighton@terenine.com>
2013-09-25 12:23:34 -0400

    WIP - switching from comp on phrases to comp on paragraphs

--- comparator.rb
+++ comparator.rb
@@ -2,12 +2,12 @@ require_relative 'xfile.rb'
 
 class Comparator
   attr_reader :aligned_pairs, :overall_confidence_score
-  attr_accessor :text_expansion_factor, :file1, :file2
+  attr_accessor :text_expansion_factor, :from_file, :to_file
 
-  def initialize(file1, file2)
-    puts "\nComparing #{file1.filename}, #{file2.filename}"
-    @file1 = file1
-    @file2 = file2
+  def initialize(from_file, to_file)
+    puts "\nComparing #{from_file.filename}, #{to_file.filename}"
+    @from_file = from_file
+    @to_file = to_file
     @overall_confidence_score = 0
     @aligned_pairs = []
     @text_expansion_factor = 1.20  # en to fr - will want to read this in from file?
@@ -15,14 +15,31 @@ class Comparator
   end
 
   def compare_structures
-#    @file1.phrase_length_array.each do |i in 0..@file1.phrase_length_array.length|
-    (0..@file1.phrase_length_array.length).each do |i|
-#      if @file1.phrase_length_array
-      puts "\n#{i}:  #{@file1.phrase_length_array[i]}   #{@file2.phrase_length_array[i]}"
+    (0..@from_file.phrase_length_array.length).each do |i|
+#      if @from_file.phrase_length_array
+      puts "\n#{i}:  #{@from_file.phrase_length_array[i]}   #{@to_file.phrase_length_array[i]}"
+      x = compare_pair(from_file.phrase_length_array[i], to_file.phrase_length_array[i])
+      puts "\t#{x}"
     end
   end
-  
+
+  def compare_pair(from_item, to_item)
+#    delta = to_item[1] / from_item[1].to_f
+    puts "\t-- #{from_item}   #{to_item}   #{from_item[1] * (1-((@text_expansion_factor-1)*6))}    #{from_item[1] * (1-(@text_expansion_factor-1))}
+       #{from_item[1]*@text_expansion_factor}   #{from_item[1] * (1+((@text_expansion_factor-1)*6))}"
+    # will definitely want to redo this...this just gives rough values based on standard deviations
+    if ( (to_item[1] > (from_item[1] * (1-(@text_expansion_factor-1)))) && (to_item[1] < (from_item[1] * @text_expansion_factor)) )
+      1.0
+    elsif ( (to_item[1] > (from_item[1] * (1-((@text_expansion_factor-1)*2)))) && (to_item[1] < (from_item[1] * (1+((@text_expansion_factor-1)*2)))) )
+      0.5
+    elsif ( (to_item[1] > (from_item[1] * (1-((@text_expansion_factor-1)*6)))) && (to_item[1] < (from_item[1] * (1+((@text_expansion_factor-1)*6)))) )
+      0.25
+    else
+      0.0
+    end
+ end
+    
   def generate_confidence_score
-    @file2.word_count / (@file1.word_count * @text_expansion_factor)
+    @to_file.word_count / (@from_file.word_count * @text_expansion_factor)
   end
 end
